93841f749838cd7933564753be310eb1d32ca6e6
furqan <applw@Applws-Mac-mini.local>
2023-08-01 05:22:33 +0500

    update and install node dependencies

--- social-node-serverless/hello-world/app.ts
+++ social-node-serverless/hello-world/app.ts
@@ -1,14 +1,13 @@
+/*
+  - eBook Example Project:
+  - Go Serverless Fast & Furious- A Cost Effective TechStack: Node.js + AWS Cloud
+  - - AWS SAM Template Project
+  - - Backend Integration: MongoDB
+ 
+ */
+
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
